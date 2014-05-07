f1770e13a44f3b4bbc1eb0e1e47a5c282b80c19b
Jamal <jwheeler604@gmail.com>
2014-05-06 21:56:20 -0400

    motion program

---
+++ humanmotion.ino
@@ -0,0 +1,19 @@
+int LED = 7;
+int sensor = 5;
+
+void setup()
+{
+	pinMode(LED, OUTPUT);
+	pinMode(sensor, INPUT);
+}
+
+void loop()
+{
+	if(digitalRead(sensor) == HIGH)
+	{
+		digitalWrite(LED, HIGH)	
+	}
+	else
+	{
+		digitalWrite(LED, LOW);
+	}
