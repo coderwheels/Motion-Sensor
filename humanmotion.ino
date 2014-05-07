1f76835c74a626aa7eb680c258fd2e63d57aff3e
Jamal <jwheeler604@gmail.com>
2014-05-06 23:40:38 -0400

    human motion

--- humanmotion.ino
+++ humanmotion.ino
@@ -1,10 +1,12 @@
 int LED = 7;
 int sensor = 5;
+int buzzer = 6;
 
 void setup()
 {
 	pinMode(LED, OUTPUT);
 	pinMode(sensor, INPUT);
+	pinMode(buzzer, OUTPUT);
 }
 
 void loop()
@@ -12,9 +14,14 @@ void loop()
 	if(digitalRead(sensor) == HIGH)
 	{
 		digitalWrite(LED, HIGH);
+		digitalWrite(buzzer, HIGH);
+		delay(5);
+		digitalWrite(buzzer, LOW);
+		delay(5);
 	}
 	else
 	{
 		digitalWrite(LED, LOW);
+		digitalWrite(buzzer, LOW);
 	}
 }
